c264c9e2b4906ea567d9680f277b8185b5e4206e
nailBestas <bestasnail@gmail.com>
2025-09-18 15:05:47 +0300

    python da String ifalerin ne oldgu ornekle aciklandi

---
+++ 01_Python_Matematik_ve_Istatistik/1_Python/2-string.py
@@ -0,0 +1,11 @@
+#string ifadeler yani dizeler
+#string ifadeler demek karakterler demek 
+#yani kisaca : metinsel verileri temsil etmek için kullanılan bir veri türünü ifade eder
+string = "hello world" # burasi karakter dizini dir
+string2 = "100" + "100"
+print(string)
+print(type(string))#string degiskenin turunu bize verir
+print(len(string))#string degiskenin karakter uzunlugunu bize verir
+print(string2)
+print(len(string2))
+print(type(string2))
