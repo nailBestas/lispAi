4996a2829ea5144fce9bbf72fbfaaa4c0ae3ece3
nailBestas <bestasnail@gmail.com>
2025-09-18 15:08:36 +0300

    python ornek eklendi

--- 01_Python_Matematik_ve_Istatistik/1_Python/2-string.py
+++ 01_Python_Matematik_ve_Istatistik/1_Python/2-string.py
@@ -2,7 +2,7 @@
 #string ifadeler demek karakterler demek 
 #yani kisaca : metinsel verileri temsil etmek için kullanılan bir veri türünü ifade eder
 string = "hello world" # burasi karakter dizini dir
-string2 = "100" + "100"
+string2 = "100" + "100" #burada tirnak isaretleri arasindaki sayilar toplanmaz birlesir cunku tirnak icindeki ifadeler string olarak tanimlanir
 print(string)
 print(type(string))#string degiskenin turunu bize verir
 print(len(string))#string degiskenin karakter uzunlugunu bize verir
